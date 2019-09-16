726c4ce552ba51bd69ee7745abebc9a12e048d17
Xylitolwhc <whcshanghai@gmail.com>
2019-09-16 13:55:55 +0800

    Task4(CNN) Finished

--- Task4/task4.py
+++ Task4/task4.py
@@ -35,17 +35,17 @@ def load_data():
     return x_train_shift, y_train_augmented, test_images, test_labels
 
 
-def shuffle_bath(x, y, batch_size):
-    rnd_idx = np.random.permutation(len(x))
-    n_batches = len(x) // batch_size
-    for batch_idx in np.array_split(rnd_idx, n_batches):
-        x_batch, y_batch = x[batch_idx], y[batch_idx]
-        yield x_batch, y_batch
+# def shuffle_bath(x, y, batch_size):
+#     rnd_idx = np.random.permutation(len(x))
+#     n_batches = len(x) // batch_size
+#     for batch_idx in np.array_split(rnd_idx, n_batches):
+#         x_batch, y_batch = x[batch_idx], y[batch_idx]
+#         yield x_batch, y_batch
 
 
 def create_model():
     model = tf.keras.Sequential([
-        tf.keras.layers.Reshape((28, 28, 1), name='reshape1'),
+        tf.keras.layers.Reshape((28, 28, 1)),
         tf.keras.layers.Conv2D(12, [3, 3], strides=1, padding='SAME', name='conv1',
                                input_shape=[28, 28, 1]),
         tf.keras.layers.MaxPooling2D(pool_size=[3, 3], strides=2, name='pool1'),
@@ -62,53 +62,53 @@ def create_model():
     return model
 
 
-def train(X_train, Y_train, X_test, Y_test):
-    tf.compat.v1.reset_default_graph()
-    with tf.name_scope('placeholders'):
-        X = tf.compat.v1.placeholder(np.float32, shape=[None, 28, 28], name='X')
-        X_reshaped = tf.reshape(X, [-1, 28, 28, 1], name='X_reshaped')
-        print(X_reshaped.shape)
-        y = tf.compat.v1.placeholder(np.int32, shape=None, name='y')
-
-    with tf.name_scope('conv'):
-        conv1 = tf.layers.Conv2D(X_reshaped, filters=12, kernel_szie=[3, 3], strides=1, padding='SAME',
-                                 name='conv1')
-        pool1 = tf.layers.MaxPooling2D(conv1, [3, 3], strides=2, name='pool1')
-
-        conv2 = tf.layers.Conv2D(pool1, 16, [3, 3], strides=1, padding='SAME', name='conv2')
-        pool2 = tf.layers.MaxPooling2D(conv2, [3, 3], strides=2, name='pool2')
-
-        pool2_flatten = tf.reshape(pool2, shape=(-1, 6 * 6 * 16))
-
-        fc1 = tf.layers.Dense(pool2_flatten, 256, activation=tf.nn.relu, name='fc1')
-        # fc2 = tf.layers.dense(fc1, 100, activation=tf.nn.relu, name='fc2')
-
-        logits = tf.layers.Dense(fc1, 10, activation=tf.nn.relu, name='output')
-
-    with tf.name_scope('loss'):
-        xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y)
-        loss = tf.reduce_mean(xentropy)
-
-    with tf.name_scope('train'):
-        optimizer = tf.compat.v1.train.AdamOptimizer(0.001)
-        training_op = optimizer.minimize(loss)
-
-    with tf.name_scope('eval'):
-        correct = tf.nn.in_top_k(logits, y, 1)
-        accuracy = tf.reduce_mean(tf.cast(correct, np.float32))
-
-    with tf.compat.v1.Session() as session:
-        session.run(tf.compat.v1.global_variables_initializer())
-        out = []
-        for epoch in range(20):
-            for x_batch, y_batch in shuffle_bath(X_train, Y_train, batch_size):
-                session.run(training_op, feed_dict={X: x_batch, y: y_batch})
-            if epoch % 1 == 0:
-                batch_acc = accuracy.eval(feed_dict={X: x_batch, y: y_batch})
-                val_acc = accuracy.eval(feed_dict={X: X_test, y: Y_test})
-                print(epoch, "Batch Acc = ", batch_acc, "Validation Acc = ", val_acc)
-                outputs = session.run(logits, feed_dict={X: X_test})
-                out.append(outputs)
+# def train(X_train, Y_train, X_test, Y_test):
+#     tf.compat.v1.reset_default_graph()
+#     with tf.name_scope('placeholders'):
+#         X = tf.compat.v1.placeholder(np.float32, shape=[None, 28, 28], name='X')
+#         X_reshaped = tf.reshape(X, [-1, 28, 28, 1], name='X_reshaped')
+#         print(X_reshaped.shape)
+#         y = tf.compat.v1.placeholder(np.int32, shape=None, name='y')
+#
+#     with tf.name_scope('conv'):
+#         conv1 = tf.layers.Conv2D(X_reshaped, filters=12, kernel_szie=[3, 3], strides=1, padding='SAME',
+#                                  name='conv1')
+#         pool1 = tf.layers.MaxPooling2D(conv1, [3, 3], strides=2, name='pool1')
+#
+#         conv2 = tf.layers.Conv2D(pool1, 16, [3, 3], strides=1, padding='SAME', name='conv2')
+#         pool2 = tf.layers.MaxPooling2D(conv2, [3, 3], strides=2, name='pool2')
+#
+#         pool2_flatten = tf.reshape(pool2, shape=(-1, 6 * 6 * 16))
+#
+#         fc1 = tf.layers.Dense(pool2_flatten, 256, activation=tf.nn.relu, name='fc1')
+#         # fc2 = tf.layers.dense(fc1, 100, activation=tf.nn.relu, name='fc2')
+#
+#         logits = tf.layers.Dense(fc1, 10, activation=tf.nn.relu, name='output')
+#
+#     with tf.name_scope('loss'):
+#         xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y)
+#         loss = tf.reduce_mean(xentropy)
+#
+#     with tf.name_scope('train'):
+#         optimizer = tf.compat.v1.train.AdamOptimizer(0.001)
+#         training_op = optimizer.minimize(loss)
+#
+#     with tf.name_scope('eval'):
+#         correct = tf.nn.in_top_k(logits, y, 1)
+#         accuracy = tf.reduce_mean(tf.cast(correct, np.float32))
+#
+#     with tf.compat.v1.Session() as session:
+#         session.run(tf.compat.v1.global_variables_initializer())
+#         out = []
+#         for epoch in range(20):
+#             for x_batch, y_batch in shuffle_bath(X_train, Y_train, batch_size):
+#                 session.run(training_op, feed_dict={X: x_batch, y: y_batch})
+#             if epoch % 1 == 0:
+#                 batch_acc = accuracy.eval(feed_dict={X: x_batch, y: y_batch})
+#                 val_acc = accuracy.eval(feed_dict={X: X_test, y: Y_test})
+#                 print(epoch, "Batch Acc = ", batch_acc, "Validation Acc = ", val_acc)
+#                 outputs = session.run(logits, feed_dict={X: X_test})
+#                 out.append(outputs)
 
 
 def shift_image(image, dx, dy):
